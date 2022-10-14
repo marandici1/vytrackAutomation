5409b7d8e2ee5274e453c0002efd63557a6ee961
Volkan Oguz <oquzvolkan@gmail.com>
2022-10-13 22:39:05 -0500

    Test Base created

---
+++ src/test/java/com/vytrack/tests/TestBase/TestBase.java
@@ -0,0 +1,24 @@
+package com.vytrack.tests.TestBase;
+
+import com.vytrack.utilities.WebDriverFactory;
+import org.openqa.selenium.WebDriver;
+import org.testng.annotations.AfterMethod;
+import org.testng.annotations.BeforeMethod;
+
+import java.util.concurrent.TimeUnit;
+
+public class TestBase {
+    private static WebDriver driver;
+    @BeforeMethod
+    public void SetUp(){
+        driver= WebDriverFactory.getDriver("Chrome");
+        driver.manage().window().maximize();
+        driver.manage().timeouts().implicitlyWait(10, TimeUnit.SECONDS);
+    }
+
+    @AfterMethod
+    public void tearDown(){
+        driver.quit();
+    }
+
+}
