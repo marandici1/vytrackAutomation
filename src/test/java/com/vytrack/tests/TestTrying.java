a6359dd4ea0929714fc32606a72833b0119cd14e
Volkan Oguz <oquzvolkan@gmail.com>
2022-11-04 11:23:36 -0500

    TestTrying Classes updated.

--- src/test/java/com/vytrack/tests/TestTrying.java
+++ src/test/java/com/vytrack/tests/TestTrying.java
@@ -3,5 +3,7 @@ package com.vytrack.tests;
 public class TestTrying {
     public static void main(String[] args) {
         System.out.println("Hello World");
+        System.out.println("newWorld");
+
     }
 }
